16478d96c7dc59a617d159d4bf98140fefdde46c
vovalodymyr <117485606+vovalodymyr@users.noreply.github.com>
2023-11-05 18:43:09 +0100

    Add files via upload

---
+++ Advanced C#/Advanced n1/HRAdministrationAPI/FactoryPattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_n1
+{
+    public static class FactoryPattern<K,T> where T : class, K, new()
+    {
+        public static K GetInstance()
+        {
+            K objk;
+            objk = new T();
+            return objk;
+        }
+    }
+}
